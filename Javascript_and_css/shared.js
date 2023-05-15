73240571491c564ce0558ccd6c41ea36bdfa5db0
mason park <joung4342@gmail.com>
2023-05-15 20:53:59 +0900

    햄버거 메뉴 이벤트 추가

--- Javascript_and_css/shared.js
+++ Javascript_and_css/shared.js
@@ -4,6 +4,8 @@ const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 const modalNoButton = document.querySelector('.modal__action--negative');
 const planButtons = document.querySelectorAll('.plan button');
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
 
 planButtons.forEach(button => {
     button.addEventListener('click', (e) => {
@@ -11,9 +13,16 @@ planButtons.forEach(button => {
         modal.style.display = 'block';
     });
 });
-
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', ()=>{
+    mobileNav.style.display = 'none';
+    closeModal();
+});
 modalNoButton.addEventListener('click', closeModal);
+
+toggleButton.addEventListener('click', () => {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
 
 function closeModal() {
     backdrop.style.display = 'none';
